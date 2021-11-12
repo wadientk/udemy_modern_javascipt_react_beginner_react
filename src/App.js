9a3c576b242acf4e3c976abd4f1c71081ca8b492
wadientk <wadientk@gmail.com>
2021-11-12 16:49:49 +0900

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <div class="input-area common-style">
+        <input id="add-text" placeholder="input task here, please" />
+        <button id="add-button">Add</button>
+      </div>
+
+      <div class="undone-area common-style">
+        <p class="area-title">undone</p>
+        <ul>
+          <li>
+            <div class="list-row">
+              <p>Learning react</p>
+              <button>Complete</button>
+              <button>Delete</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div class="done-area common-style">
+        <p class="area-title">done</p>
+        <ul>
+          <li>
+            <div class="list-row">
+              <p>Learning Angular</p>
+              <button>mark undone</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
